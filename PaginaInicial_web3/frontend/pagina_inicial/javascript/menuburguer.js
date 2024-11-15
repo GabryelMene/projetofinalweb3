b35838d4cee37b45f496f75c938916fc0c320330
gabryel <gabryelnevada@gmail.com>
2024-11-14 21:31:17 -0300

    Atualizacao do header, mais responsivo e com menu interativo

---
+++ PaginaInicial_web3/frontend/pagina_inicial/javascript/menuburguer.js
@@ -0,0 +1,36 @@
+function MenuOpenAndClose_m() {
+    const menu = document.getElementById("div-options-menu_m");
+    if (menu.style.display == "block") {
+        menu.style.display = "none"; 
+    } else {
+        menu.style.display = "block"
+    }
+}
+
+function MenuOpenAndClose_pc() {
+    const menu = document.getElementById("div-options-menu_pc");
+    if (menu.style.display == "block") {
+        menu.style.display = "none"; 
+    } else {
+        menu.style.display = "block"
+    }
+}
+
+addEventListener("resize", () => {
+    const heightOutput = window.innetHeight; 
+    const widthOutput = window.innerWidth;
+    const menu_mobile = document.getElementById("div-options-menu_m");
+    const menu_pc = document.getElementById("div-options-menu_pc");
+
+    if (widthOutput > 1600) {
+        menu_mobile.style.display = "none";
+    }
+
+    if (widthOutput < 1600) {
+        menu_pc.style.display = "none"; 
+    }
+});
+
+
+ 
+
