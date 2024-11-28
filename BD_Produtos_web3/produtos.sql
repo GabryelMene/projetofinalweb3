b38fe736314bfe9de5326738a3c3ae2f82987412
gabryel <gabryelnevada@gmail.com>
2024-11-28 10:59:08 -0300

    Atualização banco de dados produtos

--- BD_Produtos_web3/produtos.sql
+++ BD_Produtos_web3/produtos.sql
@@ -1,9 +1,14 @@
 USE leafdb;
 
 CREATE TABLE `tb_produtos` (
-    `id` NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `nome` VARCHAR(150) NOT NULL,
-    `descricao` VARCHAR(300), 
-    `preco` DOUBLE NOT NULL,
-
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    `nome` VARCHAR(150) NOT NULL, 
+    `marca` VARCHAR(50) NOT NULL,
+    `modelo` VARCHAR(50) NOT NULL,
+    `descricao` VARCHAR(300) NOT NULL,  
+    `preco` DOUBLE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
+
+
+SHOW TABLES; 
+
