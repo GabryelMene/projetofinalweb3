74f90fd0e831fffe7c905052b994cc34cab2939e
gabryel <gabryelnevada@gmail.com>
2024-11-29 18:58:12 -0300

    Atualização mobile

--- PaginaInicial_web3/frontend/pagina_inicial/javascript/menuburguer.js
+++ PaginaInicial_web3/frontend/pagina_inicial/javascript/menuburguer.js
@@ -2,11 +2,13 @@ function MenuOpenAndClose_m() {
     const menu = document.getElementById("div-options-menu_m");
     const header = document.getElementsByClassName("header"); 
 
-    if (menu.style.display == "block") {
-        menu.style.display = "none"; 
+    if (menu.classList.contains("div-options-menu_m_open")) {
+        menu.classList.remove("div-options-menu_m_open") 
+        menu.classList.add("div-options-menu_m_close"); 
         document.body.style.overflow = ''; 
     } else {
-        menu.style.display = "block"
+        menu.classList.remove("div-options-menu_m_close") 
+        menu.classList.add("div-options-menu_m_open"); 
         document.body.style.overflow = "hidden";
     }
 }
@@ -30,7 +32,9 @@ addEventListener("resize", () => {
     }
 
     if (widthOutput < 1600) {
-        menu_pc.style.display = "none"; 
+        menu_pc.style.display = "none";
+        menu_mobile.style.display = "block";
+        menu_mobile.style.right = "-1920px";
     }
 });
 
