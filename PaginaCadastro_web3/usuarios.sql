30ed62412fa53c14c6b1978252115a465f78c8be
CaptainGus <gustavosoaresandrade59@gmail.com>
2024-12-01 19:38:30 -0300

    Update usuarios.sql

--- PaginaCadastro_web3/usuarios.sql
+++ PaginaCadastro_web3/usuarios.sql
@@ -8,7 +8,8 @@ CREATE TABLE `tb_usuarios` (
     `descricao` VARCHAR(300),
     `endereçoProdutoRegistrado` VARCHAR(30),
     `novidades` TINYINT(1) NOT NULL,
-    `perfilPrivado` TINYINT(1) DEFAULT 0
+    `perfilPrivado` TINYINT(1) DEFAULT 0,
+    `pontos` INT NOT NULL
 )	ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO tb_usuarios(nome, email, senha, novidades)
